738d9586659ec3951a06ed522de7d47a851f8723
Tanu <tomartanu647@gmail.com>
2023-04-06 00:17:15 +0530

    page

---
+++ client/try_f/lib/initial_screens/intropg1.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class intropg1 extends StatefulWidget {
+
+
+  @override
+  State<intropg1> createState() => _intropg1State();
+}
+
+class _intropg1State extends State<intropg1> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: (
+          Image.asset('lib/images/iscrn2.jpeg',)
+      ),
+
+    );
+  }
+}
